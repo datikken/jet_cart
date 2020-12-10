5f309093b2381c8f11934f4357479f647b7f9ea1
Никита Шадыбеков <datikken@Nikitas-MacBook-Air.local>
2020-12-10 10:45:12 +0300

    dash layout

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,29 +1,72 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+    <Fragment>
+        <Breadcrumbs />
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+        <div class="dash">
+            <div class="dash_left">
+                <div class="dash_left-wrap">
+                    <div class="dash_left-wrap_item dash_active">
+                        <a class="dash_left-wrap_item-text">
+                            <span>Dashboard</span>
+                        </a>
+                        <div class="dash_left-wrap_item-icon"
+                             style="background-image: url('/images/dash/dash_user.svg')"></div>
+                    </div>
+
+                    <div class="dash_left-wrap_item">
+                        <a class="dash_left-wrap_item-text">
+                            <span>Заказы</span>
+                        </a>
+                        <div class="dash_left-wrap_item-icon"
+                             style="background-image: url('/images/dash/dash_orders.svg')"></div>
+                    </div>
+
+                    <div class="dash_left-wrap_item">
+                        <a class="dash_left-wrap_item-text">
+                            <span>Адреса</span>
+                        </a>
+                        <div class="dash_left-wrap_item-icon"
+                             style="background-image: url('/images/dash/dash_addr.svg')"></div>
+                    </div>
+
+                    <div class="dash_left-wrap_item">
+                        <a class="dash_left-wrap_item-text">
+                            <span>Заполнить аккаунт</span>
+                        </a>
+                        <div class="dash_left-wrap_item-icon"
+                             style="background-image: url('/images/dash/dash_fill.svg')"></div>
+                    </div>
+
+                    <div class="dash_left-wrap_item dash_logout">
+                        <a class="dash_left-wrap_item-text">
+                            <span>Выйти</span>
+                        </a>
+                    </div>
+
                 </div>
             </div>
+
+            <DashboardForms/>
+
         </div>
-    </app-layout>
+    </Fragment>
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
+    import MainLayout from '@/Layouts/MainLayout'
+    import TextBtn from '@/Shared/Btns/TextBtn'
+    import DashboardForms from '@/Shared/Dashboard/DashboardForms'
+    import {Fragment} from 'vue-fragment'
+    import Breadcrumbs from '@/Shared/Breadcrumbs/Breadcrumbs'
 
     export default {
+        name: "Dashboard",
+        layout: MainLayout,
         components: {
-            AppLayout,
-            Welcome,
-        },
+            TextBtn,
+            DashboardForms,
+            Fragment,
+            Breadcrumbs
+        }
     }
 </script>
